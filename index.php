414c8a32126594d981e8854b79d7fb88888b1e51
Dan-Gahlinger <49506863+Dan-Gahlinger@users.noreply.github.com>
2023-12-05 10:35:21 -0500

    Add files via upload
    should be final cleanup of script
    no metatags, no og stuff, no fake user agent,
    also trimmed all excess logging but left IP address of requestor

--- index.php
+++ index.php
@@ -1,102 +1,130 @@
 <?php
 
+// Version 27 Dec 5/2023 10:24AM EST/EDT - DG/transnet
+//
+
+// reputable news organizations for someone who randomly stumbled on this document
+$urls = array(
+                'https://www.reuters.com/',
+                'https://apnews.com/',
+                'https://www.bbc.com/',
+                'https://www.cbc.ca/',
+                'https://www.aljazeera.com/',
+                'https://www.dw.com/',
+                'https://www.afp.com/',
+                'https://www3.nhk.or.jp/nhkworld/',
+                'https://www.abc.net.au/',
+                'https://www.npr.org/',
+                'https://www.pbs.org/',
+              );
+
+$array_key = array_rand($urls, 1);
+
+//
+
 $dom = new DomDocument();
 
-if ($argc) { $url=$argv[1]; } else { $url=$_GET["r"]; };
+date_default_timezone_set("America/Toronto");
 
-//$fake_user_agent = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11";
-$fake_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0";
+$url = $urls[$array_key];
 
-//ini_set('user_agent', $fake_user_agent);
+// $url = 'r=https://www.apnews.com';
 
-$options = array('http' => array('user_agent'    => $fake_user_agent,
-				 'ignore_errors' => true),
-                 'ssl'  => array(
-		        	 'verify_peer' => false, // You could skip all of the trouble by changing this to false, but it's WAY uncool for security reasons.
-	 		         'cafile'      => '/etc/ssl/certs/ca-certificates.crt',
-		       	 	 'ciphers'     => 'HIGH:!SSLv2:!SSLv3',
-		       	     	 'disable_compression' => true)
-		 );
+// $r = 'r=https://www.apnews.com';
+$r = "r=" . $urls[$array_key];
 
-$context = stream_context_create($options);
-if (!filter_var($url, FILTER_VALIDATE_URL)) {
-    $url = 'https://www.google.com';
+$file = 'trackurls';
+
+if ($_SERVER['REQUEST_METHOD'] != "GET") {
+    // $url = 'r=https://www.apnews.com';
+    $url = $urls[$array_key];
+    $current = file_get_contents($file);
+    $current .= date("Y-m-d_H:i:s");
+    $current .= "\n";
+    $current .= "remote_addr_";
+    $current .= $_SERVER['REMOTE_ADDR'];
+    $current .= "\n";
+    file_put_contents($file, $current);  
 }
 
-$page    = file_get_contents($url, false, $context);
-
-libxml_use_internal_errors(true);
-
-if (! $dom->loadHTML($page) )  { echo "failed to load page $page\n"; }; 
-
-libxml_use_internal_errors(false);
-
-$xpath = new DOMXpath($dom);
-$heading=parseToArray($xpath,'title','h1');
-
-$tags = @get_meta_tags($url);
-
-// libxml_use_internal_errors(true); // Yeah if you are so worried about using @ with warnings
-
-$query = '//*/meta[starts-with(@property, \'og:\')]';
-$metas = $xpath->query($query);
-foreach ($metas as $meta) {
-    $property = $meta->getAttribute('property');
-    $content = $meta->getAttribute('content');
-    $rmetas[$property] = $content;
-}
-//var_dump($rmetas);
-
-$domain    = explode("/", $url,4);
-//echo "::: domain: ". $domain[2] . "\n";
-
-$domain[2] = strtoupper(preg_replace("/^www\./","", $domain[2]));
-
-//echo "::: domain: ". $domain[2] . "\n";
-//echo "\n\n";
-
-$rmetas["og:title"] = $domain[2] ." // ". $rmetas["og:title"];
-
-$heading[1] = $rmetas["og:title"];
-
-echo "<!DOCTYPE html>\n <html lang='en'>\n  <head>\n\n";
-
-echo "<!-- ". $url . "-->\n\n";
-
-echo "   <title>". $heading[1] ."</title>\n";
-echo '   <meta data-rh="true" name="description" content="'   . $tags["description"].   '">' ."\n";
-echo '   <meta data-rh="true" name="twitter:image" content="' . $tags["twitter:image"]. '">' ."\n";
-
-echo '   <meta data-hr="true" property="og:title"       content="' . $rmetas["og:title"]       . '">' ."\n";
-echo '   <meta data-hr="true" property="og:description" content="' . $rmetas["og:description"] . '">' ."\n";
-echo '   <meta data-hr="true" property="og:image"       content="' . $rmetas["og:image"]       . '">' ."\n";
-
-if ($url == "" or !preg_match('/^http/',$url) ) { 
-  echo "</head><body>\nError - bad url: '<b>$url</b>'<br>\nuse <b>". $_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['HTTP_HOST'] . explode('?', $_SERVER['REQUEST_URI'], 2)[0] . "?r=URL</b> please with http or https in url\n\n</body></html>\n";
-  exit;
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    if ($_SERVER['QUERY_STRING']) {
+	// requests are sometimes sent as urlencoded-strings with a stupid FB
+	// client tracker ID tacked on as a query string, so decode as needed and
+	// discard the tracker ID
+	$posb = strpos($url, "http");
+        $current = file_get_contents($file);
+	$current .= "\n";
+        $current .= date("Y-m-d_H:i:s");
+        $current .= "\n";
+	$current .= "remote_addr_";
+	$current .= $_SERVER['REMOTE_ADDR'];
+	$current .= "\n";
+        $current .= "_pre_process_";
+        $current .= $_SERVER['QUERY_STRING'];
+        $current .= "__";
+	$current .= "\n";
+        file_put_contents($file, $current);	
+	if ((!strlen($_SERVER['QUERY_STRING']) == 0) and (!is_null($_SERVER['QUERY_STRING'])) and (!empty($_SERVER['QUERY_STRING'])) and ($posb !== false)) {
+	  $url = $_SERVER['QUERY_STRING'];
+	}
+	if ((strlen($_SERVER['QUERY_STRING']) == 0) or (is_null($_SERVER['QUERY_STRING'])) or (empty($_SERVER['QUERY_STRING'])) or ($posb === false)) {
+	  // $url = 'r=https://www.apnews.com';
+          $url = "r=" . $urls[$array_key];
+	}
+    }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-function parseToArray($xpath,$elem1,$elem2)
-{
-    $xpathquery="//$elem1 | //$elem2";
-    $elements = $xpath->query($xpathquery);
+$url = rawurldecode($url);
+$current = file_get_contents($file);
+$current .= "\n";
+$current .= "_rawdecode_";
+$current .= $url;
+$current .= "__\n";
+file_put_contents($file, $current);
 
-    if (!is_null($elements)) {  
-        $resultarray=array();
-        foreach ($elements as $element) {
-            $nodes = $element->childNodes;
-            foreach ($nodes as $node) {
-              $resultarray[] = $node->nodeValue;
-            }
-        }
-        return $resultarray;
-}   }
+if ((strlen($url) == 0) or (is_null($url)) or (empty($url))) {
+    // $url = 'r=https://www.apnews.com';
+    $url = "r=" . $urls[$array_key];
+}
 
-?>
+$posc = strpos($url, "lang=en&fbclid=");
 
-  <script type="text/javascript" language="javascript">
-    window.location.href = "<?php echo $url; ?>";
-  </script>
-  </body></html>
+if ($posc !== false) {
+    // $url = 'r=https://www.apnews.com';
+    $url = "r=" . $urls[$array_key];
+}
 
+$pos = strpos($url, "http");
+
+if ($pos === false) {
+    $url = "r=" . $urls[$array_key];
+    // $url = 'r=https://www.apnews.com';
+}
+
+$posd = strpos($url, "?action=");
+
+if ($posd !== false) {
+    $url = "r=" . $urls[$array_key];
+    // $url = 'r=https://www.apnews.com';
+}
+
+$posr = strpos($url, "r=");
+if ($posr === false) {
+   $url = "r=" . $url;
+}
+
+$str2 = substr($url, 2);
+$url = $str2;
+
+$current = file_get_contents($file);
+$current .= "\n";
+$current .= "remote_addr_";
+$current .= $_SERVER['REMOTE_ADDR'];
+$current .= "\n";
+file_put_contents($file, $current);
+
+
+$url = "Location: " . $url;
+header($url);
+exit();
